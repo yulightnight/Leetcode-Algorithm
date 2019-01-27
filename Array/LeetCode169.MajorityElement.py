583226cc87ad09abf71b73050dcc5575ac9084be
Ming Yu <378331140@qq.com>
2019-01-27 13:46:54 +0800

    Create LeetCode169.MajorityElement.py

---
+++ Array/LeetCode169.MajorityElement.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in set(nums):
+            if nums.count(i) > len(nums)/2:
+                return i
